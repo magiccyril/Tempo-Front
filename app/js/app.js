5153b68e233b9419eacc583786d4ded1c0b327fc
Cyril G <cyril@18ruedivona.eu>
2014-08-26 18:03:09 +0200

    First commit

---
+++ app/js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tempoApp
+ * @description
+ * # tempoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tempoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'calendar'
+  ]);
