ed096a82e719b731cc85e6ef1719a657239acb8d
Cyril G <cyril@18ruedivona.eu>
2014-10-07 18:07:51 +0200

    Remove normalize, and remove last browser version of autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -147,7 +147,7 @@ module.exports = function(grunt) {
     // Add vendor prefixed styles
     autoprefixer: {
       options: {
-        browsers: ['last 1 version']
+        //browsers: ['last 2 versions']
       },
       dist: {
         files: [{
