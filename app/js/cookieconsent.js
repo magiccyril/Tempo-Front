80a189011973e1d2ce93128af5229af1cd1d7bbe
Cyril Gravelier <cyril@18ruedivona.eu>
2015-08-12 23:07:27 +0200

    External cookieconsent script

---
+++ app/js/cookieconsent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+window.cookieconsent_options = {
+  "message": "En poursuivant la navigation sur ce site, vous acceptez l’utilisation de cookies pour disposer de services adaptés à vos attentes.",
+  "dismiss": "J'accepte",
+  "learnMore": "Plus d'infos",
+  "link": null,
+  "theme": "css/app.css"
+};
